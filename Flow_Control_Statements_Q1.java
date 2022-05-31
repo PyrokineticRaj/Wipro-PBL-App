c49a021e4a5678e2c33e4eb5e0ac17cb54e21fc9
PyrokineticRaj <rajesh.kumar.das.tech@gmail.com>
2022-05-31 06:36:48 +0530

    Flow_Control_Statements q1 q2

---
+++ Flow_Control_Statements_Q1.java
@@ -0,0 +1,25 @@
+public class Flow_Control_Statements_Q1 {
+    public static void main(String[] args) {
+        checkNum(4);
+        sameDigit(23, 34);
+    }
+    
+    public static void checkNum(int a) {
+        if (a == 0) {
+            System.out.println("Zero");
+        } else if (a % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+
+    public static void sameDigit(int a, int b) {
+        if (a % 10 == b % 10) {
+            System.out.println("True");
+        }
+        else {
+            System.out.println("False");
+        }
+    }
+}
---
+++ Flow_Control_Statements_Q2.java
@@ -0,0 +1,14 @@
+public class Flow_Control_Statements_Q2 {
+    public static void main(String[] args) {
+        checkNum(3);
+    }
+
+    public static void checkNum(int a) {
+        if (a % 2 == 0) {
+            System.out.println("Even");
+        }
+        else {
+            System.out.println("Odd");
+        }
+    }
+}

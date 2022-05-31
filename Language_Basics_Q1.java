20097e00a583c88d6884b15d1f1b15e5bbfce4e5
PyrokineticRaj <rajesh.kumar.das.tech@gmail.com>
2022-05-31 06:30:00 +0530

    language basics done

---
+++ Language_Basics_Q1.java
@@ -0,0 +1,5 @@
+public class Language_Basics_Q1 {
+    public static void main(String[] args) {
+        System.out.println(args[0] + " Technologies " + args[1]);
+    }
+}
--- List_Q3.java
+++ List_Q3.java
@@ -1,7 +1,3 @@
-import java.util.Vector;
-
 public class List_Q3 {
-    public static void main(String[] args) {
-        
-    }
+    
 }
